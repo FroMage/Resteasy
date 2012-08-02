da0679bc8655675bcf5bd8cfb2f0d41bfe7d159c
Stephane Epardaud <stef@epardaud.fr>
2012-08-02 18:05:02 +0200

    jsclient: Fix for RESTEASY-727: handle array values

--- jaxrs/resteasy-jsapi/src/main/resources/resteasy-client.js
+++ jaxrs/resteasy-jsapi/src/main/resources/resteasy-client.js
@@ -143,29 +143,39 @@ REST.Request.prototype = {
 		},
 		addCookie : function(name, value){
 			REST.log("addCookie("+name+"="+value+")");
-			this.cookies.push([name, value]);
+			REST._addToArray(this.cookies, name, value);
 		},
 		addQueryParameter : function(name, value){
 			REST.log("addQueryParameter("+name+"="+value+")");
-			this.queryParameters.push([name, value]);
+			REST._addToArray(this.queryParameters, name, value);
 		},
 		addMatrixParameter : function(name, value){
 			REST.log("addMatrixParameter("+name+"="+value+")");
-			this.matrixParameters.push([name, value]);
+			REST._addToArray(this.matrixParameters, name, value);
 		},
 		addFormParameter : function(name, value){
 			REST.log("addFormParameter("+name+"="+value+")");
-			this.formParameters.push([name, value]);
+			REST._addToArray(this.formParameters, name, value);
 		},
 		addHeader : function(name, value){
 			REST.log("addHeader("+name+"="+value+")");
-			this.headers.push([name, value]);
+			REST._addToArray(this.headers, name, value);
 		}
 }
 
 REST.log = function(string){
 	if(REST.loglevel > 0)
 		print(string);
+}
+
+REST._addToArray = function(array, name, value){
+	if(value instanceof Array){
+		for (var i = 0; i < array.length; i++) {
+			array.push([name, array[i]]);
+		}
+	}else{
+		array.push([name, value]);
+	}
 }
 
 REST._complete = function(request, callback){
